d19e44fbb8b61315fef671572b309ac6f0ba184a
pheranmie <okekunletobi@gmail.com>
2024-02-01 18:11:27 +0100

    made adjeustment and did connected fe to contract

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contractABI from "./abi.json";
+import { useState } from "react";
 
+const ethers = require("ethers");
+const contractAddress = "0xa7B6AE7004c7Ce070D92404a9e71b51c53d87dD7";
 function App() {
+  const [ageInput, setAgeInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  async function getContractEntity() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      try {
+        const message = await contract.getEntityDetails();
+
+        // alert the message
+        console.log(message);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  }
+
+  const modifyName = async () => {
+    if (!nameInput) return;
+
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      try {
+        const txn = await contract.updateName(nameInput);
+        await txn.wait();
+        console.log("New Name Set!");
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  };
+  const modifyAge = async () => {
+    if (!ageInput) return;
+
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      try {
+        const txn = await contract.updateAge(ageInput);
+        await txn.wait();
+        console.log("New Age Set!");
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simple Registry</h1>
+        <p>Simple Registry</p>
+      </div>
+      <div>
+        <h2>View Details</h2>
+        <button onClick={getContractEntity}>Read User Registry</button>
+      </div>
+      <hr />
+      <div>
+        <h2>Set Details</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            onChange={(event) => setNameInput(event.target.value)}
+          />
+        </div>
+        <button onClick={modifyName}>Modify Name Record</button>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your Age"
+            onChange={(event) => setAgeInput(event.target.value)}
+          />
+        </div>
+        <button onClick={modifyAge}>Modify Age Record</button>
+      </div>
     </div>
   );
 }
